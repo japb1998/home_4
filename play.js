eedecf0610f49909b9e1d7437416a398d36eff60
Javier <javierperez@javiers-mbp.home>
2020-10-08 23:57:55 -0400

    almost working

--- play.js
+++ play.js
@@ -1,18 +1,19 @@
 var questionEl = document.querySelector('.question');
 var answerEl = document.querySelector('.answers');
-var btnNext = document.querySelector('.next');
-var btnPrev = document.querySelector('.prev');
 var btnStart = document.querySelector('.start');
 var controlsContainer = document.querySelector('.control')
-var timerCount = 60;
-var scoreCount=0;
+var playerInput = document.querySelector('#player')
 var timerEl = document.querySelector('.timer');
 var countEl = document.querySelector('.score')
 var body = document.body;
 var questionNumber = 0;
+var timerCount = 60;
+var scoreCount = 0;
 var setTime2;
-var quiz = [
-    {
+var playerName;
+var playersList = [];
+var storedPlayer = {};
+var quiz = [{
         question: "Wich one of this is an html tag?",
         answers: ["<p>", "<hello>", "<bye>", "<create>"],
         correct: 0
@@ -40,28 +41,35 @@ function next() {
     questionNumber++;
     if (questionNumber < quiz.length) {
         generateQuestion();
-         countEl.textContent = scoreCount;} 
-         else { 
-            if(scoreCount > 2 ){
-             answerEl.innerHTML= `<h1> your score is: ${scoreCount} out of 4 YOU PASSED!!!</h1>`;
-            } 
-            else { answerEl.innerHTML=`<h1> your score is: ${scoreCount} out of 4 TRY AGAIN!!!</h1>`;
-          
-            controlsContainer.style.display = 'block';
+        countEl.textContent = scoreCount;
+    } else {
+        if (scoreCount > 2) {
+            answerEl.innerHTML = `<h1> your score is: ${scoreCount} out of 4 YOU PASSED!!!</h1>`;
+        } else {
+            answerEl.innerHTML = `<h1> your score is: ${scoreCount} out of 4 TRY AGAIN!!!</h1>`;
+
+
         }
-         }
+        controlsContainer.style.display = 'block';
+    }
 }
 
 //Function to make the timer work
 function setTime() {
-    if (timerCount > 0) { 
+    if (timerCount > 0) {
         generateQuestion();
         var setTime2 = setInterval(function () {
             timerCount--;
             if (timerCount == 0 || questionNumber >= 4) {
+
                 clearInterval(setTime2);
                 var player = document.createElement('li');
-                player.innerHTML= `hey: ${scoreCount}`;
+                //adding keys to the empty object
+                storedPlayer.player = playerName;
+                storedPlayer.score = scoreCount;
+                playersList.push(storedPlayer)
+                localStorage.setItem('pastPlayer', JSON.stringify(playersList));
+                player.innerHTML = `${playerName}: ${scoreCount}`;
                 document.querySelector('.players').appendChild(player);
             }
             timerEl.textContent = timerCount;
@@ -69,12 +77,18 @@ function setTime() {
     }
 };
 //function for the start button it set everything to default; 
-function startQuiz (){
-    timerCount= 60;
-    scoreCount= 0;
-    questionNumber=0;
-    controlsContainer.style.display= 'none';
-    setTime();
+function startQuiz() {
+    if (playerInput.value) {
+        timerCount = 60;
+        scoreCount = 0;
+        questionNumber = 0;
+        playerName = playerInput.value
+        playerInput.value = "";
+        controlsContainer.style.display = 'none';
+        setTime();
+    } else {
+        alert('You need a name to start playing')
+    }
 }
 //eventListener for the start button 
 btnStart.addEventListener('click', startQuiz)
@@ -99,15 +113,15 @@ function generateQuestion() {
                     console.log('correct')
                     scoreCount++;
                     countEl.textContent = scoreCount;
-                        answerTest.classList.add('correct');
-                        setTimeout(next, 500)
+                    answerTest.classList.add('correct');
+                    setTimeout(next, 500)
 
-                } else { 
-countEl.textContent = scoreCount;
+                } else {
+                    countEl.textContent = scoreCount;
                     console.log('incorrect')
                     timerCount -= 10;
                     answerTest.classList.add('incorrect');
-                        setTimeout(next, 500)
+                    setTimeout(next, 500)
 
 
                 }
